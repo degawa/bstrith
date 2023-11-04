10ad66969cc22825e9e4c5f4cc4e4338e43ac8fa
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-11-04 15:33:07 +0900

    added procedure to compute logical or

--- src/bstrith.f90
+++ src/bstrith.f90
@@ -4,13 +4,14 @@ module bstrith
     use :: bstrith_logical_xor
     use :: bstrith_logical_not
     use :: bstrith_logical_and
+    use :: bstrith_logical_or
     use :: bstrith_comparison_equal
     use :: bstrith_bit_shift
     use :: bstrith_util_literal
     implicit none
     private
     public :: add, sub
-    public :: xor, and, not
+    public :: xor, and, not, or
     public :: is_equal, is_not_equal
     public :: shift, shift_l, shift_r
     public :: bstrith_zero, bstrith_one
